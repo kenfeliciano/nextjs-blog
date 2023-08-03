e1c91ee5f2ec13f1125421a3e177c565e0c79a79
Ken Feliciano <kafeliciano@gmail.com>
2023-08-03 15:39:37 -0700

    Run codemod new-link
    It added semi-colons and who knows what else.
    I saved the two files and they went away.
    In a larger project, it would be better to run a script I imagine.

--- components/layout.js
+++ components/layout.js
@@ -36,17 +36,15 @@ const Layout = ({ children, home }) => {
         ) : (
           <div>
             <Link href='/'>
-              <a>
-                <img
-                  src='/images/user-pic.jpg'
-                  className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-                  alt={name}
-                />
-              </a>
+              <img
+                src='/images/user-pic.jpg'
+                className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+                alt={name}
+              />
             </Link>
             <h2 className={utilStyles.headingLg}>
-              <Link href='/'>
-                <a className={utilStyles.colorInherit}>{name}</a>
+              <Link href='/' className={utilStyles.colorInherit}>
+                {name}
               </Link>
             </h2>
           </div>
@@ -55,9 +53,7 @@ const Layout = ({ children, home }) => {
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
-          <Link href='/'>
-            <a>← Back to home</a>
-          </Link>
+          <Link href='/'>← Back to home</Link>
         </div>
       )}
     </div>
